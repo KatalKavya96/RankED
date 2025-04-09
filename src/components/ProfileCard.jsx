8818dcaec2a3d0318d454ace24f863ec878aa3d9
Kavya Katal <kavyakatal09@gmail.com>
2025-04-09 23:49:02 +0530

    Commit

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import {useUser} from "../components/UserContext"
 
 const ProfileCard = () => {
+    const { user } = useUser();
+
   return (
     
     <>
         <div className='w-full h-[410px] max-w-xs bg-white rounded-xl p-4 mx-auto md:mx-0 border border-black/25 shadow-xl'>
 
             <div className='flex items-center justify-between p-2 my-5'>
-                <img className='w-20 h-20 bg-blue-600/15 border border-blue-300 rounded-xl'></img>
+                <img
+                src={user?.photoURL || 'https://i.postimg.cc/SKfhtdnJ/avatar-placeholder.png'}
+                alt="Profile"
+                className='w-20 h-20 bg-blue-600/15 border border-blue-300 rounded-xl object-cover'
+                />  
                 <div>
-                    <h1 className='text-black/80'>Kavya Katal</h1>
+                    <h1 className='text-black/80'>{user?.displayName || 'No Name'}</h1>
                     <p className='text-black/60'>KavyaKatal96</p>
                 </div>
             </div>
@@ -19,7 +26,7 @@ const ProfileCard = () => {
             <div className='flex flex-col justify-center my-10 gap-5 mx-2'>
                 <h1 className='text-black/75 text-m'>India</h1>
                 <h1 className='text-black/75 text-m'>Newton School of Technology</h1>
-                <h1 className='text-black/75 text-m'>KatalKavya96</h1>
+                <h1 className='text-black/75 text-m'>{user?.email || 'No Email'}</h1>
                 <h1 className='text-black/75 text-m'><a href=''>LinkedIn</a></h1>
             </div>
         </div>
